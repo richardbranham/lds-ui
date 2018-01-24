889f41fa86bf45ccefb6fa8b1b54eae0b2d7f84e
richard-tkg <richard-tkg@github.com>
2018-01-24 06:12:28 -0500

    Changes

---
+++ src/app/pages/user-location/user-location.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserLocationComponent } from './user-location.component';
+
+const routes: Routes = [
+  {
+    path: 'locations',
+    component: UserLocationComponent
+  }
+];
+
+export const routing = RouterModule.forChild(routes);
