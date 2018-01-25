fbd2924518cbce33b352123e4558112845a64c21
richard-tkg <richard-tkg@github.com>
2018-01-25 07:45:35 -0500

    Video event handling is working.

--- src/app/pages/view-content/view-content.component.ts
+++ src/app/pages/view-content/view-content.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { VgAPI } from 'videogular2/core';
 
 @Component({
   selector: 'view-content',
-  //template: `<vg-player (onPlayerReady)="onPlayerReady($event)">
-  template: `<vg-player>
-    <video #media [vgMedia]="media" id="singleVideo" preload="auto" width="800" height="600" controls>
-        <source [src]="contentUrl" type="video/mp4">
+  template: `<vg-player (onPlayerReady)="onPlayerReady($event)">
+    <video #media [vgMedia]="media" id="singleVideo" preload="auto" width="400" height="300" controls>
+        <source src="http://ldsapi.kotter.net/storage/UserStatusChanges.mp4" type="video/mp4">
     </video>
   </vg-player>`,
 })
 
 export class ViewContentComponent {
   constructor(private http: HttpClient) {
-
   }
 
   contentUrl: string = "";
+  api:VgAPI;
 
   ngOnInit() {
     try {
@@ -34,21 +34,39 @@ export class ViewContentComponent {
         );
 
         const trainingProgressRequest = this.http.post('http://ldsapi.kotter.net/api/training/getprogress', 
-          {"userId":"1", "contentId":"23915430-016d-11e8-81bf-01b3b67b09bc"})
-        .subscribe(
+            {"userId":"1", "contentId":"23915430-016d-11e8-81bf-01b3b67b09bc"})
+          .subscribe(
           res => {
             //let v = JSON.parse(res);
             console.log("res", res);
           },
           err => {
-            console.log('Error occured in getprogress');
+            console.log('Error occured in getprogress', err);
           }
         );
       } catch (error) {
       // This error is usually called when device does not support geolocation at all
       alert(error);
-    }
-  }  
+    } // catch
+  } // ngOnInit
+
+
+  onPlayerReady(api:VgAPI) {
+    this.api = api;
+    console.log("onPlayerReady");
+
+    this.api.getDefaultMedia().subscriptions.pause.subscribe(
+      () => {
+          console.log("Video paused");
+      }
+    );
+    
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(
+      () => {
+          console.log("Video ended.");
+      }
+    );
+  } // onPlayerReady
 }
 
 // https://akveo.github.io/ng2-admin/articles/013-create-new-page/
