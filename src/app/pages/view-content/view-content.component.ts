493be09c7a218eb2fc8227b34f9fb1b6404c9d18
Richard Branham <richard@kotter.net>
2018-01-24 20:37:05 -0500

    Training content management.

--- src/app/pages/view-content/view-content.component.ts
+++ src/app/pages/view-content/view-content.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'view-content',
   //template: `<vg-player (onPlayerReady)="onPlayerReady($event)">
   template: `<vg-player>
     <video #media [vgMedia]="media" id="singleVideo" preload="auto" width="800" height="600" controls>
-        <source src="http://ldsapi.kotter.net/storage/UserStatusChanges.mp4" type="video/mp4">
+        <source [src]="contentUrl" type="video/mp4">
     </video>
   </vg-player>`,
 })
+
 export class ViewContentComponent {
-  constructor() {}
+  constructor(private http: HttpClient) {
+
+  }
+
+  contentUrl: string = "";
+
+  ngOnInit() {
+    try {
+      const req = this.http.get('http://ldsapi.kotter.net/api/training/getcontent', {responseType: 'text'})
+        .subscribe(
+          res => {
+            //let v = JSON.parse(res);
+            console.log("res", res);
+            this.contentUrl = "http://ldsapi.kotter.net" + res;
+            console.log("contentUrl", this.contentUrl);
+          },
+          err => {
+            console.log('Error occured');
+          }
+        );
+    } catch (error) {
+      // This error is usually called when device does not support geolocation at all
+      alert(error);
+    }
+  }  
 }
 
 // https://akveo.github.io/ng2-admin/articles/013-create-new-page/
